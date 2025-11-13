427cc664566a103c3bafb04a9e60bb546e373322
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2025-11-13 18:06:53 +0530

    loop

---
+++ code5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int time = 22;
+    if(time<20)
+     cout<<"Good Morning";
+     else if(time<10)
+     cout<<"Hello";
+     else
+     cout<<"GTA V";
+
+     return 0;
+}

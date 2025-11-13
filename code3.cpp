041a208de7a22ed11c4a8886916a39ad35209b28
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2025-11-13 18:01:49 +0530

    basic

---
+++ code3.cpp
@@ -0,0 +1,13 @@
+// to compare two numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x = 20;
+    int y = 18;
+    if (x>y)
+
+    {
+        cout <<"x is greater than y";
+}
+}

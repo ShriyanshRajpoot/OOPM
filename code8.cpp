3bf94c8c6a85dd0e7b303e7def6aca79a3657e36
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2025-11-14 19:07:06 +0530

    loop

---
+++ code8.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int mynum = 8;
+    
+    if (mynum % 2 == 0) {
+        cout << mynum << "is even.\n";
+    } else {
+        cout << mynum << "is odd.\n";
+    }
+}
